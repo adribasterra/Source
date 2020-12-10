4cb886ea0c0dd3b1589ed025f7957f59c8082a2d
adribasterra <adrianabasterraa@gmail.com>
2020-12-10 17:00:11 +0100

    TimeManager update

--- GameEngine/TimeManager.cpp
+++ GameEngine/TimeManager.cpp
@@ -5,8 +5,20 @@
 /*****************************************************************************/
 void TimeManager::Update(void)
 {
-	time += deltaTime;
 	//Set deltaTime depending on the last time rendered.
+	time += deltaTime;
+	currentTime = SDL_GetTicks();
+	deltaTime = currentTime - lastTimeRendered;
+
+	//Calculate and correct fps
+	avgFPS = numFrames / (SDL_GetTicks() / 1000.f);
+	if (avgFPS > 2000000)
+	{
+		avgFPS = 0;
+	}
+
+	++numFrames;
+	lastTimeRendered = currentTime;
 }
 
 /*****************************************************************************/
